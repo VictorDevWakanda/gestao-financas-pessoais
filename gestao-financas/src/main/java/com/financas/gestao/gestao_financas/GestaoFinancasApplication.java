3e083c229191c16f61ed97d25a30e40ab8d48a06
Victor <victormota_03@hotmail.com>
2025-06-08 22:13:32 -0300

    first commit

---
+++ gestao-financas/src/main/java/com/financas/gestao/gestao_financas/GestaoFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.financas.gestao.gestao_financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFinancasApplication.class, args);
+	}
+
+}
---
+++ gestao-financas/src/test/java/com/financas/gestao/gestao_financas/GestaoFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financas.gestao.gestao_financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
