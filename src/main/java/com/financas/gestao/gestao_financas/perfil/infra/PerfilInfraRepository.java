065c36979b1acce0be77f386cbd6514073195bd5
Victor <victormota_03@hotmail.com>
2025-07-10 21:04:44 -0300

    Implementa postPerfil

--- src/main/java/com/financas/gestao/gestao_financas/perfil/infra/PerfilInfraRepository.java
+++ src/main/java/com/financas/gestao/gestao_financas/perfil/infra/PerfilInfraRepository.java
@@ -5,16 +5,22 @@ import org.springframework.stereotype.Repository;
 import com.financas.gestao.gestao_financas.perfil.application.repository.PerfilRepository;
 import com.financas.gestao.gestao_financas.perfil.domain.Perfil;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Repository
+@RequiredArgsConstructor
 @Log4j2
-public class PerfilInfraRepository implements PerfilRepository{
+public class PerfilInfraRepository implements PerfilRepository {
+
+    private final PerfilSpringDataJPARepository perfilSpringDataJPARepository;
 
     @Override
     public Perfil salvaPerfil(Perfil perfil) {
-        log.info("Salvando perfil: {}", perfil);
-        return null;
+        log.info("[start] PerfilInfraRepository - salvaPerfil");
+        perfilSpringDataJPARepository.save(perfil);
+        log.info("[finish] PerfilInfraRepository - salvaPerfil");
+        return perfil;
     }
 
 }
